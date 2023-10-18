25cba00eb446b73fd1128856d319a86a4c0a1e4c
gacrichards <g.a.cole.richards@gmail.com>
2023-10-17 23:10:53 -0600

    topics update

--- client/topics.py
+++ client/topics.py
@@ -1,6 +1,7 @@
 #topics
 prefix = "103123"
 motion_alarm = prefix + "/motion"
+presence_alarm = prefix + "/presence"
 proxy_alarm = prefix + "/proxy"
 touch_alarm = prefix + "/touch"
 measurement_alarm = prefix + "/measurement"
